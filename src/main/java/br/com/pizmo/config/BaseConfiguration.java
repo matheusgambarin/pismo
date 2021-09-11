258c7821e28718b788da84ce174e0ec139fc12bb
Matheus Gambarin <matheus.gambarin@elotech.com.br>
2021-09-11 14:51:31 -0300

    refactory após atualização gitignore

--- src/main/java/br/com/pizmo/config/BaseConfiguration.java
+++
@@ -1,9 +0,0 @@
-package br.com.pizmo.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EntityScan(basePackages = "br/com/pizmo/domain")
-public class BaseConfiguration {
-}
--- src/main/java/br/com/pizmo/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pizmo.repository;
-
-import br.com.pizmo.domain.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
